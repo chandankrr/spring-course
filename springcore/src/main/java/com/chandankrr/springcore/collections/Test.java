0e5191fc641edeb371149bc3aa64c116d7405ad6
Chandan Kumar <chandankrr.91@gmail.com>
2023-12-20 11:09:28 +0530

    learned about to inject collections type using xml

---
+++ springcore/src/main/java/com/chandankrr/springcore/collections/Test.java
@@ -0,0 +1,18 @@
+package com.chandankrr.springcore.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/chandankrr/springcore/collections/collectionconfig.xml");
+		
+		Employee employee1=(Employee) context.getBean("employee1");
+		
+		System.out.println(employee1.getName());
+		System.out.println(employee1.getPhones());
+		System.out.println(employee1.getAddresses());
+		System.out.println(employee1.getCourses());
+	}
+}
