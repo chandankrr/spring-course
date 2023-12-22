f6bfb3b6238c3142b6db892b5298d8f06fa513fc
Chandan Kumar <chandankrr.91@gmail.com>
2023-12-22 13:22:40 +0530

    learned about spring expression language

---
+++ springcore/src/main/java/com/chandankrr/springcore/spel/Demo.java
@@ -0,0 +1,58 @@
+package com.chandankrr.springcore.spel;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo {
+	@Value("#{22 + 11}")
+	private int x;
+	@Value("#{33 > 22 ? 50 : 0}")
+	private int y;
+	@Value("#{T(java.lang.Math).sqrt(25)}")
+	private double z;
+	@Value("#{T(java.lang.Math).E}")
+	private double e;
+	@Value("#{new java.lang.String('Chandan Kumar')}")
+	private String name;
+	@Value("#{8 > 3 ? true : false}")
+	private boolean isActive;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getE() {
+		return e;
+	}
+	public void setE(double e) {
+		this.e = e;
+	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	@Override
+	public String toString() {
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", name=" + name + ", isActive=" + isActive
+				+ "]";
+	}
+	
+}
---
+++ springcore/src/main/java/com/chandankrr/springcore/spel/Test.java
@@ -0,0 +1,15 @@
+package com.chandankrr.springcore.spel;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/chandankrr/springcore/spel/config.xml");
+		
+		Demo d1 = context.getBean("demo", Demo.class);
+		System.out.println(d1);
+	}
+
+}
