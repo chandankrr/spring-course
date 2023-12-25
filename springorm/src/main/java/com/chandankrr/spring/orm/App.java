9fb4d64f1cf944f6ec767b6d06c8058d753fad10
Chandan Kumar <chandankrr.91@gmail.com>
2023-12-25 15:35:47 +0530

    created a console based menu driven application to perform CRUD opeations with database

--- springorm/src/main/java/com/chandankrr/spring/orm/App.java
+++ springorm/src/main/java/com/chandankrr/spring/orm/App.java
@@ -1,5 +1,9 @@
 package com.chandankrr.spring.orm;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,11 +13,120 @@ import com.chandankrr.spring.orm.entities.Student;
 public class App {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/chandankrr/spring/orm/config.xml");
-		
+
 		StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
-		
-		Student student = new Student(113, "Chandan Kumar", "Patna");
-		int result = studentDao.insert(student);
-		System.out.println("inserted student_id: " + result);
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		boolean isTrue = true;
+
+		do {
+			System.out.println("PRESS 1 for add new student");
+			System.out.println("PRESS 2 for display all students");
+			System.out.println("PRESS 3 for get details of single student");
+			System.out.println("PRESS 4 for delete student");
+			System.out.println("PRESS 5 for update student");
+			System.out.println("PRESS 6 for exit");
+
+			try {
+				System.out.println("\nEnter your choice: ");
+				int choice = Integer.parseInt(br.readLine());
+
+				switch (choice) {
+				case 1:
+					System.out.println("Enter user id: ");
+					int id = Integer.parseInt(br.readLine());
+					System.out.println("Enter user name: ");
+					String name = br.readLine();
+					System.out.println("Enter user city: ");
+					String city = br.readLine();
+
+					Student student = new Student();
+					student.setStudentId(id);
+					student.setStudentName(name);
+					student.setStudentCity(city);
+
+					int result = studentDao.insert(student);
+					System.out.println("Student inserted with id: " + result);
+					System.out.println("***********************************");
+					System.out.println();
+					break;
+				case 2:
+					System.out.println("----------Students Detail-----------");
+					List<Student> allStudents = studentDao.getAllStudents();
+					for (Student s : allStudents) {
+						System.out.println(s);
+					}
+					System.out.println("***********************************");
+					System.out.println();
+					break;
+				case 3:
+					System.out.println("Enter student id: ");
+					int studentId1 = Integer.parseInt(br.readLine());
+					Student studentData = studentDao.getStudent(studentId1);
+					System.out.println(studentData);
+					System.out.println("***********************************");
+					System.out.println();
+					break;
+				case 4:
+					System.out.println("Enter student id: ");
+					int studentId2 = Integer.parseInt(br.readLine());
+					studentDao.deleteStudent(studentId2);
+					System.out.println("Student deleted with id: " + studentId2);
+					System.out.println("***********************************");
+					System.out.println();
+					break;
+				case 5:
+					System.out.println("Enter student id: ");
+					int studentId3 = Integer.parseInt(br.readLine());
+
+					System.out.println("Enter 1 to update name");
+					System.out.println("Enter 2 to update city");
+
+					int enteredValue = Integer.parseInt(br.readLine());
+
+					// Get the existing student data
+					Student studentData1 = studentDao.getStudent(studentId3);
+
+					if (studentData1 == null) {
+						System.out.println("Student not found!");
+						break;
+					}
+
+					if (enteredValue == 1) {
+						System.out.println("Enter new name: ");
+						String newName = br.readLine();
+						studentData1.setStudentName(newName);
+					} else if (enteredValue == 2) {
+						System.out.println("Enter new city: ");
+						String newCity = br.readLine();
+						studentData1.setStudentCity(newCity);
+					} else {
+						System.out.println("Invalid option!");
+						break;
+					}
+
+					// Update the student in the database
+					studentDao.updateStudent(studentData1);
+					System.out.println("Student information updated successfully.");
+					System.out.println("***********************************");
+					System.out.println();
+					break;
+				case 6:
+					isTrue = false;
+					break;
+				default:
+					System.out.println("Invalid choice! Please enter a valid option.");
+					break;
+				}
+
+			} catch (Exception e) {
+				System.out.println("Invalid input!!");
+				System.out.println(e.getMessage());
+			}
+		} while (isTrue);
+
+		System.out.println("\nThankyou for using my application \nsee you soon!!");
+
 	}
 }
--- springorm/src/main/java/com/chandankrr/spring/orm/dao/StudentDao.java
+++ springorm/src/main/java/com/chandankrr/spring/orm/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.chandankrr.spring.orm.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -9,7 +11,7 @@ import com.chandankrr.spring.orm.entities.Student;
 public class StudentDao {
 
 	private HibernateTemplate hibernateTemplate;
-	
+
 	// save student data
 	@Transactional
 	public int insert(Student student) {
@@ -18,11 +20,36 @@ public class StudentDao {
 		return result;
 	}
 
+	// get the single data(object) of student
+	public Student getStudent(int studentId) {
+		Student student = hibernateTemplate.get(Student.class, studentId);
+		return student;
+	}
+	
+	// get all data of students
+	public List<Student> getAllStudents() {
+		List<Student> students = hibernateTemplate.loadAll(Student.class);
+		return students;
+	}
+	
+	// delete student
+	@Transactional
+	public void deleteStudent(int studentId) {
+		Student student = hibernateTemplate.get(Student.class, studentId);
+		hibernateTemplate.delete(student);
+	}
+	
+	// update student details
+	@Transactional
+	public void updateStudent(Student student) {
+		hibernateTemplate.update(student);
+	}
+
 	public HibernateTemplate getHibernateTemplate() {
 		return hibernateTemplate;
 	}
 
 	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
 		this.hibernateTemplate = hibernateTemplate;
-	}	
+	}
 }
